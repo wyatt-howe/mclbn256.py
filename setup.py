e92906a154a3fb52e91966ee546f17f9b5f5373b
Wyatt Howe <whowe@magnite.com>
2022-06-10 18:06:37 -0400

    lib

--- setup.py
+++ setup.py
@@ -1,16 +1,1 @@
-from setuptools import setup, dist
-
-class BinaryDistribution(dist.Distribution):
-    def is_pure(self):
-        return False
-
-setup(
-    # name='mclbn256',
-    # package_data={'lib': ['libmclbn256.dylib']},
-    # package_data={'mclbn256': ['../lib/libmclbn256.dylib', '../lib/libmclbn256.dll']},
-    # package_data={'mclbn256': ['libmclbn256.dylib']},
-    # data_files=['libmclbn256.dylib'],
-    # include_package_data=True,
-    distclass=BinaryDistribution,
-    # packages=['mclbn256'],
-)
+import setuptools as s;s.setup()
